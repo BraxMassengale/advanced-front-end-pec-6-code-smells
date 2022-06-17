42345aceb576f70550979acaf1fa7c27e321a50b
Braxton Zane Massengale <bmassengale@uoc.edu>
2022-06-17 22:49:41 +0200

    Added refactoring example 4

---
+++ Refactoring4.ts
@@ -0,0 +1,26 @@
+// ANTES
+
+class Recipt {
+
+    totalCost: number;
+    serviceCost: number;
+    materialsCost: number;
+
+}
+
+
+// DESPUÉS
+
+class Recipt {
+
+    totalCost: number;
+    serviceCost: number;
+    materialsCost: number;
+
+}
+
+class ServiceCallRecipt {
+
+    serviceCost: number;
+
+}
