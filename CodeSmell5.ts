1551e98b1203da55b08034841a27daa784500c6f
Braxton Zane Massengale <bmassengale@uoc.edu>
2022-06-17 22:10:50 +0200

    Added code smell 5

---
+++ CodeSmell5.ts
@@ -0,0 +1,13 @@
+// ANTES
+
+numberIsEven(num: number): boolean {
+    return (num % 2 == 0 ? true : false);
+    return false;
+}
+
+
+// DESPUÉS
+
+numberIsEven(num: number): boolean {
+    return (num % 2 == 0 ? true : false);
+}
